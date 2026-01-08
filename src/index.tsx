c676e95174e70dc52ee1b13ca0c2eb60eceb7656
SeptianSamdany <227007072@student.unsil.ac.id>
2026-01-08 21:01:50 +0700

    fix some issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { I18nextProvider } from "react-i18next";
+import 'antd/dist/antd.min.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+import Router from "./router";
+import i18n from "./translation";
+
+const App = () => (
+  <BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <Router />
+    </I18nextProvider>
+  </BrowserRouter>
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
