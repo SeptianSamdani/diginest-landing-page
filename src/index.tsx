59d8a68298cebfe016ecddb979d9222ffaacb713
SeptianSamdany <227007072@student.unsil.ac.id>
2026-01-08 20:55:28 +0700

    add some features

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom";
-import { I18nextProvider } from "react-i18next";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import Router from "./router";
-import i18n from "./translation";
-
-const App = () => (
-  <BrowserRouter>
-    <I18nextProvider i18n={i18n}>
-      <Router />
-    </I18nextProvider>
-  </BrowserRouter>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
-
-ReactDOM.render(<App />, document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
